0e0b67d11a310eb8cc725513316424aa049b07ed
NataliaNovi <nata.novikova.v@gmail.com>
2025-11-16 21:54:41 +0300

    homework20 commit

---
+++ src/main/kotlin/ru/stimmax/lessons/lesson20/homeworks/Array.kt
@@ -0,0 +1,10 @@
+package ru.stimmax.lessons.lesson20.homeworks
+
+// 1 Определите функцию-расширение для массива чисел, которая не принимает аргументов и возвращает пару из чисел - первое и последнее. Если массив был пуст то вернуть пару из null значений.
+fun Array<Int>.getAround(): Pair<Int?, Int?> {
+    return if (isEmpty()) {
+        null to null
+    } else {
+        first() to last()
+    }
+}
---
+++ src/main/kotlin/ru/stimmax/lessons/lesson20/homeworks/ListExtensions.kt
@@ -0,0 +1,16 @@
+package ru.stimmax.lessons.lesson20.homeworks
+
+// 2 Создайте функцию-расширение для изменяемого списка элементов:
+//1.с дженериком T ограниченным интерфейсом Comparable<T>
+//2.которая принимает булево значение
+//3.возвращает этот же список только в виде неизменяемого
+//4.сам изменяемый список при этом должен стать отсортированным по возрастанию, если в функцию передано true и по убыванию, если false (используем функции sort() и sortDescending()
+fun <T : Comparable<T>> MutableList<T>.sort(direction: Boolean): List<T> {
+    val self = toList()
+    if (direction) {
+        sort()
+    } else {
+        sortDescending()
+    }
+    return self
+}
